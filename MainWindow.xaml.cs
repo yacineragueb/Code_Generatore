4e8aea3ca4231e183f5a4e78ea20e11580624d5d
Yacine Ragueb <yacineddd32@gmail.com>
2026-05-15 17:06:28 +0100

    feat(ui, db): feat: refactor control styles; start SQL Server connection implementation; improve password visibility toggle behavior

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
 
 namespace Code_Generatore
 {
@@ -16,9 +17,64 @@ namespace Code_Generatore
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string _server = "localhost";
+        private string _connectionString;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ShowPasswordCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            PasswordUnmaskTextBox.Text = PasswordBox.Password;
+            PasswordUnmaskTextBox.Visibility = Visibility.Visible;
+            PasswordBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowPasswordCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Password = PasswordUnmaskTextBox.Text;
+            PasswordUnmaskTextBox.Visibility = Visibility.Collapsed;
+            PasswordBox.Visibility = Visibility.Visible;
+        }
+
+        private void ConnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string Username = UsernameTextBox.Text;
+            string Passowrd = PasswordBox.Password;
+
+
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Passowrd))
+            {
+                ErrorText.Text = "Username and Password are required.";
+                ErrorText.Visibility = Visibility.Visible;
+                return;
+            }
+
+            ErrorText.Visibility = Visibility.Collapsed;
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+            {
+                DataSource = _server,
+                UserID = Username,
+                Password = Passowrd,
+                TrustServerCertificate = true
+            };
+
+            _connectionString = builder.ConnectionString;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    conn.Open();
+                    MessageBox.Show("Connection successful!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection failed: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
