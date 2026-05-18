0a066ff543568c3118da62ad79a5f26775177509
Yacine Ragueb <yacineddd32@gmail.com>
2026-05-18 14:06:25 +0100

    feat(ui): implement live clock in windCode_gen;

--- windCode_gen.xaml.cs
+++ windCode_gen.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Threading;
 
 namespace Code_Generatore
 {
@@ -19,15 +8,37 @@ namespace Code_Generatore
     /// </summary>
     public partial class windCode_gen : Window
     {
-        private MainWindow _loginWindow;
+        private readonly MainWindow _loginWindow;
+        private DispatcherTimer? _timer;
+
+        private void StartLiveClock()
+        {
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            _timer.Tick += (s, e) => UpdateClock();
+            UpdateClock();
+            _timer.Start();
+        }
+
         public windCode_gen(MainWindow loginWindow)
         {
             InitializeComponent();
             _loginWindow = loginWindow;
+
+            StartLiveClock();
+        }
+
+        private void UpdateClock()
+        {
+            CurrentDateTimeTextBlock.Content = DateTime.Now.ToString("dd MMMM, yyyy - HH:mm:ss");
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
+            _timer?.Stop();
             _loginWindow.Show();
         }
     }
